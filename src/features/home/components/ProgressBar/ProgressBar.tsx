ed36a40692c4b80175c517acf9f2702a1f70272a
Maheresio <zamalek.545r5@gmail.com>
2026-06-02 15:08:14 +0300

    refactor (home) :
    add animation to progressbar

--- src/features/home/components/ProgressBar/ProgressBar.tsx
+++ src/features/home/components/ProgressBar/ProgressBar.tsx
@@ -1,25 +1,54 @@
-import useTheme from '@/theme/useTheme';
-import { LinearGradient } from 'expo-linear-gradient';
+import { useEffect } from 'react';
 import { Text, View } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  withTiming,
+  Easing,
+} from 'react-native-reanimated';
+
+import useTheme from '@/theme/useTheme';
 import createProgressBarStyles from './ProgressBar.styles';
 import { useCompletedTodos, useTodos } from '../../api/useTodos';
+
+const AnimatedLinearGradient = Animated.createAnimatedComponent(LinearGradient);
 
 const ProgressBar = () => {
   const { colors } = useTheme();
   const styles = createProgressBarStyles(colors);
 
-  const completedTodosCount = useCompletedTodos()?.length ?? 0;
-  const getTodoCount = useTodos()?.length ?? 0;
+  const todos = useTodos();
 
-  const progressPercentage = getTodoCount > 0 ? (completedTodosCount / getTodoCount) * 100 : 0;
+  const totalTodosCount = todos?.length ?? 0;
+
+  const completedTodosCount = useCompletedTodos()?.length ?? 0;
+
+  const progressPercentage =
+    totalTodosCount > 0 ? (completedTodosCount / totalTodosCount) * 100 : 0;
+
+  const progress = useSharedValue(0);
+
+  useEffect(() => {
+    progress.value = withTiming(progressPercentage, {
+      duration: 500,
+      easing: Easing.in(Easing.elastic(0.5)),
+    });
+  }, [progressPercentage]);
+
+  const animatedStyle = useAnimatedStyle(() => {
+    return {
+      width: `${progress.value}%`,
+    };
+  });
 
   return (
     <View style={styles.progressBarContainer}>
       <View style={styles.progressBar}>
-        <LinearGradient
+        <AnimatedLinearGradient
           colors={colors.gradients.success}
-          style={{ ...styles.progressFill, width: `${progressPercentage}%` }}
-        ></LinearGradient>
+          style={[styles.progressFill, animatedStyle]}
+        />
       </View>
 
       <Text style={styles.progressText}>{Math.round(progressPercentage)}%</Text>
